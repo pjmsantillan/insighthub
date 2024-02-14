a5d5776f4466de2a053da8070e1d39a483e63db0
Phillip <santilanphillip23@gmail.com>
2024-02-14 21:45:03 +0800

    Second Commit

--- src/components/global/Main.js
+++ src/components/global/Main.js
@@ -1,5 +1,4 @@
 import Headlines from '../sub-components/Headline';
-import LatestNewscards from '../sub-components/LatestNewscard';
 import Navigation from '../sub-components/Navigation'
 
 
@@ -8,8 +7,8 @@ const Main =()=>{
     return (
         <div className="Main">
             <Headlines></Headlines>
-             <Navigation></Navigation>
-            <LatestNewscards></LatestNewscards>
+            <Navigation></Navigation>
+       
         </div>
     )
 }
